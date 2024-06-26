fc25562f704e15f2640cb51ee64406e7498e26be
Савченко Даниил <sada@nita.ru>
2024-06-26 16:34:30 +0300

    Engine update
    - Swapped material sphere for plane
    - Added parallax mapping control

--- demo/entry.cpp
+++ demo/entry.cpp
@@ -30,36 +30,37 @@ void LoadMaterial(GR::GrayEngine* Context, int ID)
 		Context->BindImage(Context->GetComponent<GRComponents::NormalMap>(object), "content\\brick_normal.jpg", GR::EImageType::RGBA_UNORM);
 		Context->BindImage(Context->GetComponent<GRComponents::RoughnessMap>(object), "content\\brick_roughness.jpg", GR::EImageType::RGBA_UNORM);
 		Context->BindImage(Context->GetComponent<GRComponents::AmbientMap>(object), "content\\brick_ao.jpg", GR::EImageType::RGBA_UNORM);
+		Context->BindImage(Context->GetComponent<GRComponents::DisplacementMap>(object), "content\\brick_height.jpg", GR::EImageType::RGBA_UNORM);
 		break;
 	case 1:
 		Context->BindImage(Context->GetComponent<GRComponents::AlbedoMap>(object), "content\\concrete_albedo.jpg", GR::EImageType::RGBA_SRGB);
 		Context->BindImage(Context->GetComponent<GRComponents::NormalMap>(object), "content\\concrete_normal.jpg", GR::EImageType::RGBA_UNORM);
 		Context->BindImage(Context->GetComponent<GRComponents::RoughnessMap>(object), "content\\concrete_roughness.jpg", GR::EImageType::RGBA_UNORM);
 		Context->BindImage(Context->GetComponent<GRComponents::AmbientMap>(object), "content\\concrete_ao.jpg", GR::EImageType::RGBA_UNORM);
+		Context->BindImage(Context->GetComponent<GRComponents::DisplacementMap>(object), "content\\concrete_height.jpg", GR::EImageType::RGBA_UNORM);
 		break;
 	case 2:
 		Context->BindImage(Context->GetComponent<GRComponents::AlbedoMap>(object), "content\\metal_albedo.jpg", GR::EImageType::RGBA_SRGB);
 		Context->BindImage(Context->GetComponent<GRComponents::NormalMap>(object), "content\\metal_normal.jpg", GR::EImageType::RGBA_UNORM);
 		Context->BindImage(Context->GetComponent<GRComponents::RoughnessMap>(object), "content\\metal_roughness.jpg", GR::EImageType::RGBA_UNORM);
 		Context->BindImage(Context->GetComponent<GRComponents::AmbientMap>(object), "content\\metal_ao.jpg", GR::EImageType::RGBA_UNORM);
+		Context->BindImage(Context->GetComponent<GRComponents::DisplacementMap>(object), "content\\metal_height.jpg", GR::EImageType::RGBA_UNORM);
 		break;
 	default:
 		break;
 	}
 }
 
-void LoadMaterialSphereScene(GR::GrayEngine* Context)
+void LoadMaterialWall(GR::GrayEngine* Context)
 {
 	Context->ClearEntities();
 
-	GRShape::Sphere Shape;
-	Shape.radius = 10.f;
-	Shape.rings = 65u;
-	Shape.slices = 65u;
+	GRShape::Plane Shape;
+	Shape.scale = 20.f;
 
 	object = Context->AddShape(Shape);
-	CameraPYR = { 0.0, glm::radians(180.0), 0.0 };
-	Context->GetMainCamera().View.SetOffset({ -2.5, 50.0, 40.0 });
+	CameraPYR = { 0.0, glm::radians(210.0), 0.0 };
+	Context->GetMainCamera().View.SetOffset({ 10.0, 50.0, 20.0 });
 	Context->GetMainCamera().View.SetRotation(CameraPYR.x, CameraPYR.y, CameraPYR.z);
 }
 
@@ -112,7 +113,7 @@ void UpdateResources(GR::GrayEngine* Context, double Delta)
 	{
 		if (SceneID == 0)
 		{
-			LoadMaterialSphereScene(Context);
+			LoadMaterialWall(Context);
 			LoadMaterial(Context, MaterialID);
 			LoadedMaterial = MaterialID;
 		}
@@ -154,10 +155,16 @@ void Loop(GR::GrayEngine* Context, double Delta)
 	
 	Context->GetMainCamera().View.Translate(off);
 
-	if (LoadedScene != 1)
+	if (LoadedScene == 0)
 	{
 		GRComponents::Transform& wld = Context->GetComponent<GRComponents::Transform>(object);
 		wld.SetOffset(TVec3(0.0, 50.0, 0.0));
+		wld.SetRotation(glm::radians(-90.0), 0.0, 0.0);
+		wld.SetOffset(TVec3(0.0, 50.0 + glm::sin(global_angle) * 2.5, 0.0));
+	}
+	else if (LoadedScene == 2)
+	{
+		GRComponents::Transform& wld = Context->GetComponent<GRComponents::Transform>(object);
 		wld.SetOffset(TVec3(0.0, 50.0 + glm::sin(global_angle) * 2.5, 0.0));
 		wld.Rotate(0.0, 0.01, 0.0);
 	}
@@ -176,7 +183,7 @@ void UI(GR::GrayEngine* Context, double Delta)
 	ImGui::Text("Common settings");
 	ImGui::Separator();
 
-	const char* Materials[3] = { "Material sphere", "R-M Spheres", "Model" };
+	const char* Materials[3] = { "Material wall", "R-M Spheres", "Model" };
 	ImGui::Combo("Scene", &SceneID, Materials, 3);
 	ImGui::SliderFloat("Sun position", &Sun, 0.0, 1.0);
 
@@ -188,6 +195,9 @@ void UI(GR::GrayEngine* Context, double Delta)
 	{
 		GRComponents::RoughnessMultiplier& RM = Context->GetComponent<GRComponents::RoughnessMultiplier>(object);
 		ImGui::SliderFloat("Roughness mult", &RM.R, 0.0, 1.0);
+
+		GRComponents::DisplacementScale& HM = Context->GetComponent<GRComponents::DisplacementScale>(object);
+		ImGui::SliderFloat("Height mult", &HM.H, 0.1, 10.0);
 
 		const char* Materials[3] = { "Brick", "Concrete", "Painted metal" };
 		ImGui::Combo("Materials", &MaterialID, Materials, 3);
