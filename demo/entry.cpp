3ab2f7f34b3d66544451b1ba5a91f613d4a24d80
Савченко Даниил <sada@nita.ru>
2024-09-02 09:19:31 +0300

    Architecture update

--- demo/entry.cpp
+++ demo/entry.cpp
@@ -1,63 +1,79 @@
 #include "pch.hpp"
-#include "Engine/engine.hpp"
+#include "imgui/imgui.h"
+#include "Engine/utils.hpp"
+#include "Engine/window.hpp"
+#include "Engine/event_listener.hpp"
 
-TVec3 CameraPYR;
-TVec2 Cursor = TVec2(0.0);
-double speed_mult = 1.0;
+using namespace GR;
+
+glm::vec3 CameraPYR;
+glm::vec2 Cursor = glm::vec2(0.0);
+std::map<EKey, EAction> KeyStates;
+CloudLayerProfile CloudLayer{};
+CloudLayerProfile CloudLayer_Old{};
+bool MousePressed = false;
+double speed_mult = 100.0;
 float Sun = 1.0;
 
-bool MousePressed = false;
-std::map<GR::EKey, GR::EAction> KeyStates;
-
-CloudLayerProfile CloudLayer{};
-
-void Loop(GR::GrayEngine* Context, double Delta)
+void MousePress(GREvent::MousePress Event, void* Data)
 {
-	static CloudLayerProfile CurrentProfile = CloudLayer;
-	constexpr double StepTime = 1.0 / 60.0;
-	const double simulation_step = StepTime / Delta;
-	double angle = glm::mod(Context->GetTime(), 360.0);
+	Window* wnd = static_cast<Window*>(Data);
+	Cursor = wnd->GetCursorPos();
+	MousePressed = (Event.action != EAction::Release);
+};
 
-	Context->GetWindow().SetTitle(("Vulkan Application " + std::format("{:.1f}", 1.0 / Delta)).c_str());
-	Context->GetRenderer().m_SunDirection = glm::normalize(glm::vec3(0.0, Sun * 2.0 - 1.0, 1.0));
-
-	TDVec3 off = TDVec3(0.0);
-	if (KeyStates[GR::EKey::A] != GR::EAction::Release) off.x += speed_mult * simulation_step;
-	if (KeyStates[GR::EKey::D] != GR::EAction::Release) off.x -= speed_mult * simulation_step;
-
-	if (KeyStates[GR::EKey::W] != GR::EAction::Release) off.z += speed_mult * simulation_step;
-	if (KeyStates[GR::EKey::S] != GR::EAction::Release) off.z -= speed_mult * simulation_step;
-
-	if (KeyStates[GR::EKey::PageUp] != GR::EAction::Release) off.y += speed_mult * simulation_step;
-	if (KeyStates[GR::EKey::PageDown] != GR::EAction::Release) off.y -= speed_mult * simulation_step;
-
-	Context->GetMainCamera().View.Translate(off);
-
-	TVec3 U = glm::normalize(TDVec3(0.0, Context->GetRenderer().Rg, 0.0) + Context->GetMainCamera().View.GetOffset());
-	TQuat p = glm::rotation(glm::vec3(0.0, 1.0, 0.0), U);
-
-	TQuat q = glm::angleAxis(CameraPYR.y, U);
-	q = q * glm::angleAxis(CameraPYR.z, p * glm::vec3(0, 0, 1));
-	q = q * glm::angleAxis(-CameraPYR.x, p * glm::vec3(1, 0, 0));
-
-	glm::mat3 M = glm::mat3_cast(q * p);
-
-	Context->GetMainCamera().View.matrix[0] = glm::dvec4(glm::normalize(M[0]), 0.0);
-	Context->GetMainCamera().View.matrix[1] = glm::dvec4(glm::normalize(M[1]), 0.0);
-	Context->GetMainCamera().View.matrix[2] = glm::dvec4(glm::normalize(M[2]), 0.0);
-
-	//printf("%f %f %f\n", Context->GetMainCamera().View.GetOffset().x, Context->GetMainCamera().View.GetOffset().y, Context->GetMainCamera().View.GetOffset().z);
-
-	if (CurrentProfile != CloudLayer)
+void MouseMove(GREvent::MousePosition Event, void* Data)
+{
+	if (MousePressed)
 	{
-		Context->GetRenderer().SetCloudLayerSettings(CloudLayer);
-		CurrentProfile = CloudLayer;
+		CameraPYR += glm::radians(glm::vec3 (Cursor.y - Event.y, Cursor.x - Event.x, 0.0));
+		Cursor = { Event.x, Event.y };
 	}
-}
+};
 
-void UI(GR::GrayEngine* Context, double Delta)
+void MouseScroll(GREvent::ScrollDelta Event, void* Data)
 {
-	ImGui::SetCurrentContext(Context->GetGUIContext());
+	speed_mult = glm::clamp(speed_mult + 100.0 * Event.y, 1.0, 10000.0);
+};
+
+void KeyPress(GREvent::KeyPress Event, void* Data)
+{
+	KeyStates[Event.key] = Event.action;
+
+	if (Event.action == GR::EAction::Press)
+	{
+		Window* wnd = static_cast<Window*>(Data);
+		switch (Event.key)
+		{
+		case GR::EKey::Key_1:
+			Sun = 1.0;
+			CloudLayer.Coverage = 0.185;
+			CloudLayer.VerticalSpan = 0.2;
+			break;
+		case GR::EKey::Key_2:
+			Sun = 0.495;
+			CloudLayer.Coverage = 0.2;
+			CloudLayer.VerticalSpan = 0.5;
+			break;
+		case GR::EKey::Key_3:
+			Sun = 0.52;
+			CloudLayer.Coverage = 0.25;
+			CloudLayer.VerticalSpan = 0.0;
+			break;
+		case GR::EKey::Key_4:
+			Sun = 0.45;
+			CloudLayer.Coverage = 0.185;
+			CloudLayer.VerticalSpan = 0.49;
+			break;
+		default:
+			break;
+		}
+	}
+};
+
+inline void UpdateUI(Renderer& renderer)
+{
+	ImGui::SetCurrentContext(renderer.GetImguiContext());
 
 	ImGui::Begin("Settings", 0, ImGuiWindowFlags_::ImGuiWindowFlags_AlwaysAutoResize);
 	ImGui::SetWindowPos({ 0, 0 });
@@ -71,82 +87,87 @@ void UI(GR::GrayEngine* Context, double Delta)
 	ImGui::DragFloat("Absorption", &CloudLayer.Absorption, 1e-5, 0.0, 1.0, "%.5f");
 
 	ImGui::End();
-}
+};
 
-void KeyPress(GR::GrayEngine* Context, GREvent::KeyPress Event)
+inline void ControlCamera(GR::Camera& camera, double delta)
 {
-	KeyStates[Event.key] = Event.action;
+	glm::dvec3 off = glm::dvec3(0.0);
+	if (KeyStates[EKey::A] != EAction::Release) off.x += speed_mult * delta;
+	if (KeyStates[EKey::D] != EAction::Release) off.x -= speed_mult * delta;
 
-	if (Event.action == GR::EAction::Press)
+	if (KeyStates[EKey::W] != EAction::Release) off.z += speed_mult * delta;
+	if (KeyStates[EKey::S] != EAction::Release) off.z -= speed_mult * delta;
+
+	if (KeyStates[EKey::PageUp] != EAction::Release) off.y += speed_mult * delta;
+	if (KeyStates[EKey::PageDown] != EAction::Release) off.y -= speed_mult * delta;
+
+	camera.View.Translate(off);
+
+	glm::vec3 U = glm::normalize(glm::dvec3(0.0, Renderer::Rg, 0.0) + camera.View.GetOffset());
+	glm::quat p = glm::rotation(glm::vec3(0.0, 1.0, 0.0), U);
+
+	glm::quat q = angleAxis(CameraPYR.y, U);
+	q = q * glm::angleAxis(CameraPYR.z, p * glm::vec3(0, 0, 1));
+	q = q * glm::angleAxis(-CameraPYR.x, p * glm::vec3(1, 0, 0));
+
+	glm::mat3 M = glm::mat3_cast(q * p);
+
+	camera.View.matrix[0] = glm::dvec4(glm::normalize(M[0]), 0.0);
+	camera.View.matrix[1] = glm::dvec4(glm::normalize(M[1]), 0.0);
+	camera.View.matrix[2] = glm::dvec4(glm::normalize(M[2]), 0.0);
+};
+
+inline void ControlWorld(Renderer& renderer, double delta)
+{
+	renderer.m_SunDirection = glm::normalize(glm::vec3(0.0, Sun * 2.0 - 1.0, 1.0));
+
+	if (CloudLayer_Old != CloudLayer)
 	{
-		CloudLayerProfile Profile = CloudLayer;
-		switch (Event.key)
-		{
-		case GR::EKey::Key_1:
-			Sun = 1.0;
-			Profile.VerticalSpan = 0.2;
-			Profile.Coverage = 0.185;
-			break;
-		case GR::EKey::Key_2:
-			Profile.VerticalSpan = 0.5;
-			Profile.Coverage = 0.2;
-			Sun = 0.495;
-			break;
-		case GR::EKey::Key_3:
-			Sun = 0.52;
-			Profile.VerticalSpan = 0.0;
-			Profile.Coverage = 0.25;
-			break;
-		case GR::EKey::Key_4:
-			Sun = 0.45;
-			Profile.VerticalSpan = 0.49;
-			Profile.Coverage = 0.185;
-			break;
-		default:
-			break;
-		}
-
-		CloudLayer = Profile;
+		renderer.SetCloudLayerSettings(CloudLayer);
+		CloudLayer_Old = CloudLayer;
 	}
-}
+};
 
-void MousePress(GR::GrayEngine* Context, GREvent::MousePress Event)
+int main(int argc, const char** argv)
 {
-	Cursor = Context->GetWindow().GetCursorPos();
-	MousePressed = (Event.action != GR::EAction::Release);
-}
+	// Systems setup
+	Window window(1024, 720, "Volumetric clouds demo");
+	Renderer& renderer = window.GetRenderer();
+	Camera& camera = renderer.m_Camera;
+	EventListener listener = {};
 
-void MouseMove(GR::GrayEngine* Context, GREvent::MousePosition Position)
-{
-	if (MousePressed)
+	// Events setup
+	window.SetUpEvents(listener);
+	listener.SetUserPointer(&window);
+	listener.Subscribe(MouseScroll);
+	listener.Subscribe(MousePress);
+	listener.Subscribe(MouseMove);
+	listener.Subscribe(KeyPress);
+
+	// World setup
+	renderer.m_Camera.View.SetOffset({ 0.0, 50.0, 0.0 });
+
+	// Rendering
+	double delta = 0.0;
+	auto last_time = GetTime();
+	while (window.IsAlive())
 	{
-		CameraPYR += glm::radians(TVec3(Cursor.y - Position.y, Cursor.x - Position.x, 0.0));
-		Cursor = { Position.x, Position.y };
+		// Update delta
+		auto time = GetTime();
+		delta = time - last_time;
+		window.SetTitle(("Volumetric clouds demo " + std::format("{:.1f}", 1.0 / delta)).c_str());
+		last_time = time;
+
+		// Update simulation
+		window.ProcessEvents();
+		ControlCamera(camera, delta);
+		ControlWorld(renderer, delta);
+
+		// Render frame
+		renderer.BeginFrame(delta);
+	
+		UpdateUI(renderer);
+		
+		renderer.EndFrame();
 	}
-}
-
-void MouseScroll(GR::GrayEngine* Context, GREvent::ScrollDelta Delta)
-{
-	speed_mult = glm::clamp(speed_mult + 2.5f * Delta.y, 1.0, 10000.0);
-}
-
-int main(int argc, char** argv)
-{
-	ApplicationSettings Settings = { "Vulkan Application", { 1024, 720 } };
-	std::unique_ptr<GR::GrayEngine> Engine = std::make_unique<GR::GrayEngine>(argc, argv, Settings);
-
-	Engine->AddInputFunction(Loop);
-	Engine->AddInputFunction(UI);
-
-	Engine->GetEventListener().Subscribe(KeyPress);
-	Engine->GetEventListener().Subscribe(MouseMove);
-	Engine->GetEventListener().Subscribe(MousePress);
-	Engine->GetEventListener().Subscribe(MouseScroll);
-
-	Engine->GetMainCamera().View.SetOffset({ 0.0, 50.0, 0.0 });
-	Engine->GetRenderer().SetCloudLayerSettings(CloudLayer);
-
-	Engine->StartGameLoop();
-
-	return 0;
-}
+};
